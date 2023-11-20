0129132e9aa18fff42ce454d72cb2de2895f3564
Dmitry Morozov <dmitry.morozov@relsys.eu>
2023-11-20 22:03:21 +0300

    fix

--- site/next.config.js
+++ site/next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '1337',
-            pathname: '/uploads/**',
+            protocol: 'https',
+            hostname: 'citrolux.ru',
+            port: '',
+            pathname: '/cms/uploads/**',
           },
         ],
         formats: []
