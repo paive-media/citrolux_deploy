31016f2d8b767a235a75af1a4a9cbf5ba7479e89
Dmitry Morozov <dmitry.morozov@relsys.eu>
2023-11-20 21:41:16 +0300

    fix

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '127.0.0.1'),
+  url: env("PUBLIC_URL", "https://citrolux.ru/cms"),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
