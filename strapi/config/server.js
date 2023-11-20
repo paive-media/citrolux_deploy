55abffebf0e4b2b20bc312a76fb0dd7f98f6a2a5
Dmitry Morozov <dmitry.morozov@relsys.eu>
2023-11-20 22:38:10 +0300

    fix

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   host: env('HOST', '127.0.0.1'),
   url: env("PUBLIC_URL", "https://citrolux.ru/cms"),
   port: env.int('PORT', 1337),
@@ -7,5 +7,14 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  cron: {
+    enabled: env.bool('CRON_ENABLED', true),
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*'],
+    },
   },
 });
