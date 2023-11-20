a6a597fa70a889996488f24e5072c0008ed43c18
Dmitry Morozov <dmitry.morozov@relsys.eu>
2023-11-20 22:59:24 +0300

    fix

--- site/app/api-config.ts
+++ site/app/api-config.ts
@@ -1,7 +1,7 @@
 const apiConfig = {
-    graphql_url: 'http://strapi:1337/graphql',
+    graphql_url: 'https://strapi:1337/graphql',
     rest_url: 'https//strapi:1337/api',
-    base_url: 'http://strapi:1337'
+    base_url: 'https://strapi:1337'
   };
 
   export default apiConfig
--- site/app/api/grapqlInstance.ts
+++ site/app/api/grapqlInstance.ts
@@ -1,15 +1,23 @@
-import axios from "axios";
-import apiConfig from "../api-config"
+import axios from 'axios';
+import apiConfig from '../api-config';
+import https from 'https';
 
 const GRAPHQL_URL = apiConfig.graphql_url;
 
+
+const agent = new https.Agent({
+    rejectUnauthorized: false, // Необязательно, если вы используете самоподписанные сертификаты
+    secureProtocol: 'TLSv1_method' // Установите ту версию протокола SSL, которая поддерживается сервером
+});
+
 const graphqlApi = axios.create({
-  baseURL: GRAPHQL_URL,
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  method: "POST",
+    baseURL: GRAPHQL_URL,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    httpsAgent: agent
 });
 
 export default graphqlApi;
