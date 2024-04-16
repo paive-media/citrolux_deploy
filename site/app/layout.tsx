6547e7a5d7170f8e72b7e95ad7659293695251e9
Ivan Artemiev <71406102+paive-media@users.noreply.github.com>
2024-04-16 14:46:10 +0300

    lang =ru

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="ru">
         <body className={inter.className}>
         <Script id="metrika-counter" strategy="afterInteractive">
             {`(function (m, e, t, r, i, k, a) {
